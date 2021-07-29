03792b218c9e0bc1a21e148369667d65a8964306
ivyf <iveee1023@gmail.com>
2021-07-29 15:44:00 +0800

    CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do
-      resources :users, only: [:index, :show]
-      resources :posts, only: [:index, :show, :update]
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:create]
+      end
+      resources :posts, only: [:index, :show, :update, :destroy]
     end
   end
 end
